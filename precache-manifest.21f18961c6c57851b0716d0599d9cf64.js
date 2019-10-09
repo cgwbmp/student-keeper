3917939ee064b5a7585ca623ce0bdbb80a7bf096
Alena <cgwbmp@gmail.com>
2019-10-09 09:29:39 +0500

    Updates

---
+++ precache-manifest.21f18961c6c57851b0716d0599d9cf64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e83d0528c88e8267f645d5ea6c9a51c",
+    "url": "/student-keeper/index.html"
+  },
+  {
+    "revision": "4f8494259f18f0a8ebfd",
+    "url": "/student-keeper/static/css/main.0f587efd.chunk.css"
+  },
+  {
+    "revision": "ed3f01e6ac827b8878c6",
+    "url": "/student-keeper/static/js/2.427788c2.chunk.js"
+  },
+  {
+    "revision": "4f8494259f18f0a8ebfd",
+    "url": "/student-keeper/static/js/main.462cd5e2.chunk.js"
+  },
+  {
+    "revision": "748844c8a294e660c37f",
+    "url": "/student-keeper/static/js/runtime-main.cc12ce63.js"
+  }
+]);
